9c90f90f87718e3d9ee693a4fe51f859f45693e9
Denise Maia <de.maia90@gmail.com>
2019-03-22 17:20:34 -0300

    Updating columns to be not null.

--- src/main/java/api/domain/Course.java
+++ src/main/java/api/domain/Course.java
@@ -6,6 +6,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
 
 @Data
 @Entity
@@ -14,6 +15,8 @@ public class Course {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
+
+    @NotNull
     private String name;
 
     protected Course(){
--- src/main/java/api/domain/Student.java
+++ src/main/java/api/domain/Student.java
@@ -6,6 +6,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
 
 @Data
 @Entity
@@ -14,7 +15,10 @@ public class Student {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
+
+    @NotNull
     private String name;
+
     private String registerNumber;
 
     protected Student(){
